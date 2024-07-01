64b72bb0f190420cbe7bac86caa2353a00a0419b
AlesciDavide <davi.alesci@Gmail.com>
2024-07-01 14:54:53 +0200

    sistemate le task

--- index.php
+++ index.php
@@ -5,12 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="./css/style.css">
     <title>php-todolist-json</title>
 </head>
 <body>
 <main id="app">
-        <section v-for="list in todoList">
-            {{ list }}
+        <section class="task" >
+            <article class="singleTask" v-for="list in todoList">
+                <h1>
+                    {{ list.task }}
+                </h1>
+                <p>
+                    {{ list.description }}
+                </p>
+                <span>
+                    {{ list.completato }}
+                </span>
+            </article>
         </section>
 
 </main>
